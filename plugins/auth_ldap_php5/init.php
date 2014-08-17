0061dd081f7ed7ba2f1a678cad6eee63791d50b6
Niels Walhof <nawalhof@itnature.nl>
2014-08-17 08:45:05 +0200

    version 0.02

--- plugins/auth_ldap_php5/init.php
+++ plugins/auth_ldap_php5/init.php
@@ -3,7 +3,7 @@
  * 	Tiny Tiny RSS plugin for LDAP authentication
  * 	@author Stormbyte (nawalhof@itnature.nl)
  * 	Requires php5-ldap
- * 	@version 0.01
+ * 	@version 0.02
  * 
  */
  
@@ -23,7 +23,6 @@
 /**
  * 	Notes -
  * 	LDAP configuration tested on Zentyal 3.5 Samba LDAP server.
- * 	The users created will have a prefix 'ldapuser_'
  * 	The userlist is cached but the password is always validated against LDAP server
  * 
  * 	ToDo - 
@@ -42,20 +41,29 @@ class Auth_Ldap_Php5 extends Plugin implements IAuthModule {
 			$baseDN = null,
 			$searchFilter = null,
 			$userDN = null,
+			$userDisplayName = null,
+			$userMail = null,
 			$ldapConnection = null;
-			
+			    
 	function init($host) {
 		$this->host = $host;
-		$this->base = new Auth_Base();
-		$this->owner_uid = '2';
-		$this->dbh = $host->get_dbh();
-		
 		$host->add_hook($host::HOOK_AUTH_USER, $this);
+		
+		$this->base = new Auth_Base();
+		$this->owner_uid = '2'; // Owner of the LDAP data
+		$this->dbh = $host->get_dbh();
 		$this->load_data();
+		
+		$this->serverUri 	= (defined('LDAP_AUTH_SERVER_URI'))?LDAP_AUTH_SERVER_URI:null;
+        $this->bindDN 		= (defined('LDAP_AUTH_BINDDN'))?LDAP_AUTH_BINDDN:null;
+        $this->bindPW 		= (defined('LDAP_AUTH_BINDPW'))?LDAP_AUTH_BINDPW:null;
+        $this->baseDN 		= (defined('LDAP_AUTH_BASEDN'))?LDAP_AUTH_BASEDN:null;
+        
+        $this->searchFilter = (defined('LDAP_AUTH_SEARCHFILTER'))?LDAP_AUTH_SEARCHFILTER:null;
 	}
 
 	function about() {
-		return array(0.01, 
+		return array(0.02, 
 			"Authenticate against LDAP server", 
 			"Stormbyte", 
 			true);
@@ -68,13 +76,6 @@ class Auth_Ldap_Php5 extends Plugin implements IAuthModule {
 	 */
 	function authenticate($login, $password) {
 		if ($login && $password) {
-			$this->serverUri 	= (defined('LDAP_AUTH_SERVER_URI'))?LDAP_AUTH_SERVER_URI:null;
-	        $this->bindDN 		= (defined('LDAP_AUTH_BINDDN'))?LDAP_AUTH_BINDDN:null;
-	        $this->bindPW 		= (defined('LDAP_AUTH_BINDPW'))?LDAP_AUTH_BINDPW:null;
-	        $this->baseDN 		= (defined('LDAP_AUTH_BASEDN'))?LDAP_AUTH_BASEDN:null;
-	        
-	        $this->searchFilter = (defined('LDAP_AUTH_SEARCHFILTER'))?LDAP_AUTH_SEARCHFILTER:null;
-	        
 	        // Connect to LDAP
 	        if( $this->openLdap() ){
 		        // Check if User is in list
@@ -82,8 +83,27 @@ class Auth_Ldap_Php5 extends Plugin implements IAuthModule {
 		        	// Check if User and password are valid
 			        if( $this->userDN && $this->validateLdapUser( $password ) ){
 			        	// If user is valid
-						$this->closeLdap();
-			        	return $this->base->auto_create_user('ldapuser_'.$login);
+			        	$this->closeLdap();
+						
+						// Prefix userlogin not possible with api
+						// $login = 'ldapuser_'.$login;
+						
+						$user_id = $this->base->auto_create_user($login, $password);
+						
+						if ($user_id) {
+                    		// update user name
+                    		if ($this->userDisplayName){
+                    			$this->userDisplayName = db_escape_string($this->userDisplayName);
+                    			db_query("UPDATE ttrss_users SET full_name = '".$this->userDisplayName."' WHERE id = " . $user_id);
+                    		}
+                    		// update user mail
+                    		if ($this->userMail){
+                    			$this->userMail = db_escape_string($this->userMail);
+                    			db_query("UPDATE ttrss_users SET email = '".$this->userMail."' WHERE id = " . $user_id);
+                    		}
+                    		
+    			        	return $user_id;
+						}
 			        }
 		        }
 	        }
@@ -106,10 +126,10 @@ class Auth_Ldap_Php5 extends Plugin implements IAuthModule {
 		
 		if( ( $this->get( $this, 'auth_ldap_time', 0 ) + 300 ) < time() || !$ldapSearchResult ){
 			if($this->ldapConnection){
-				$ldapBind=ldap_bind( $this->ldapConnection, $this->bindDN , $this->bindPW );
+				$ldapBind = @ldap_bind( $this->ldapConnection, $this->bindDN , $this->bindPW );
 				if( $ldapBind ){
 					// performing search
-					$ldapSearch=ldap_search( $this->ldapConnection, $this->baseDN, $this->searchFilter );
+					$ldapSearch = @ldap_search( $this->ldapConnection, $this->baseDN, $this->searchFilter );
 					$ldapSearchResult = $this->processLdapResult( ldap_get_entries( $this->ldapConnection, $ldapSearch ) );
 					$this->set($this, 'auth_ldap_time', time(), false );
 					$this->set($this, 'auth_ldap_result',  $ldapSearchResult, true );
@@ -119,6 +139,8 @@ class Auth_Ldap_Php5 extends Plugin implements IAuthModule {
 		
 		if( $ldapSearchResult && isset($ldapSearchResult[$user])){
 			$this->userDN = $ldapSearchResult[$user]['dn'];
+			$this->userDisplayName = $ldapSearchResult[$user]['displayname'];
+			$this->userMail = $ldapSearchResult[$user]['mail'];
 			return true;
 		}
 		
@@ -137,6 +159,8 @@ class Auth_Ldap_Php5 extends Plugin implements IAuthModule {
 				$newAccount = array();
 				$newAccount['samaccountname'] = $ldapSearchResult[$i]['samaccountname'][0];
 				$newAccount['dn'] = $ldapSearchResult[$i]['dn'];
+				$newAccount['displayname'] = $ldapSearchResult[$i]['displayname'][0];
+				$newAccount['mail'] = $ldapSearchResult[$i]['mail'][0];
 				$result[$ldapSearchResult[$i]['samaccountname'][0]] = $newAccount;
 			}
 		}
@@ -150,7 +174,8 @@ class Auth_Ldap_Php5 extends Plugin implements IAuthModule {
 	 */
 	private function validateLdapUser( $password ){
 		if( $this->ldapConnection && $this->userDN ){
-		    if( ldap_bind($this->ldapConnection, $this->userDN , $password) ){
+            $validateUser = ldap_bind($this->ldapConnection, $this->userDN , $password);
+		    if( $validateUser ){
 		    	return true;
 		    }
 		}
@@ -164,7 +189,12 @@ class Auth_Ldap_Php5 extends Plugin implements IAuthModule {
 	 */
 	private function openLdap(){
 		if( !$this->ldapConnection && $this->serverUri){
-			$this->ldapConnection=ldap_connect($this->serverUri);
+			$this->ldapConnection = @ldap_connect($this->serverUri);
+			ldap_set_option($this->ldapConnection, LDAP_OPT_PROTOCOL_VERSION,3);
+            ldap_set_option($this->ldapConnection, LDAP_OPT_REFERRALS,0);
+            if( !ldap_errno($this->ldapConnection) == 0 ){
+                $this->closeLdap();
+            }
 		}
 		return $this->ldapConnection;
 	}
@@ -178,6 +208,7 @@ class Auth_Ldap_Php5 extends Plugin implements IAuthModule {
 		if( $this->ldapConnection ){
 			ldap_close( $this->ldapConnection );
 		}
+		$this->ldapConnection = null;
 	}
 
 	function api_version() {
